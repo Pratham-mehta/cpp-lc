efd639b32d2364434cd8afe655b54f8dafad2d89
Pratham Mehta <pm3483@nyu.edu>
2026-05-12 15:06:55 -0400

    frkn

--- basics/program1.cpp
+++ basics/program1.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 using namespace std;
-
+/*
+Write a Program that:
+1. Asks the user to enter their favorite number (between 1 and 100).
+2. Reads the input from the console.
+3. Displays a message saying, “Amazing!! That’s my favorite number too!”
+*/
 int main(){
     int num;
-    cout<<"Enter your favorite number"<<endl;
+    cout<<"Enter your favorite number between 1 and 100: "<<endl;
     cin>>num;
-    cout<<"Amazing!! That’s my favorite number too!"<<endl;
+    cout<<"Amazing!! That’s my favorite number "<<num<<" too!"<<"\n";
     return 0;
 }
